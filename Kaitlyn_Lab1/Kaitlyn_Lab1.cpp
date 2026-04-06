6507465f6dbd7f957f3d4719f5ae00ae772627cd
kthorn11 <kaitmarie713@gmail.com>
2026-04-06 10:50:17 -0500

    <Updated greeting message>

--- Kaitlyn_Lab1/Kaitlyn_Lab1.cpp
+++ Kaitlyn_Lab1/Kaitlyn_Lab1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello from Kaitlyn Thornton! This is my first Git commit";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
